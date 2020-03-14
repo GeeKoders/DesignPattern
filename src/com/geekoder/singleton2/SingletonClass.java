9b260777fe2ffe88b3f6403854b69c16358f2ab3
GeeKoders <iloveitone@gmail.com>
2020-03-14 14:21:47 +0800

    20200314 add singleton pattern 2

---
+++ src/com/geekoder/singleton2/SingletonClass.java
@@ -0,0 +1,17 @@
+package com.geekoder.singleton2;
+
+public enum SingletonClass {
+
+	INSTANCE;
+
+	private int counter;
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+
+	public int getCounter() {
+		return this.counter;
+	}
+
+}
